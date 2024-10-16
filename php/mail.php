461e5e4ced4d74ff6669e962c8d852a7ddfd55a2
Programmerid <v.mike.id@gmail.com>
2024-10-16 15:51:14 +0300

    add some blocks for lizing

--- php/mail.php
+++ php/mail.php
@@ -2,9 +2,10 @@
 if (isset($_POST['subj'])) {$subj = $_POST['subj'];}
 if (isset($_POST['text'])) {$name = $_POST['text'];}
 if (isset($_POST['tel'])) {$tel = $_POST['tel'];}
+if (isset($_POST['mail'])) {$tel = $_POST['mail'];}
+if (isset($_POST['lizing'])) {$tel = $_POST['lizing'];}
 
 $address  = 'ecc.sales@yandex.ru';
-
 $mes = "Тема: $subj\nИмя: $name\nТелефон: $tel";   
 $sub='Форма обратной связи с сайта ЕСС'; 
 $email='yess.service@yandex.ru'; 
